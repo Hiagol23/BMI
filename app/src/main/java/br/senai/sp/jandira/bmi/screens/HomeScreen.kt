325037a6c8106a184a85c17a6a356759db71eb8a
HIAGO RODRIGUES ORTOLAN <24122385@dsn127m1246731.cfp127aluno.local>
2025-03-06 09:31:12 -0300

    Tela inicial feita

--- app/src/main/java/br/senai/sp/jandira/bmi/screens/HomeScreen.kt
+++ app/src/main/java/br/senai/sp/jandira/bmi/screens/HomeScreen.kt
@@ -11,12 +11,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -24,6 +27,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,6 +36,11 @@ import br.senai.sp.jandira.bmi.R
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
+    
+    var nameState = remember { 
+        mutableStateOf("")
+    }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -107,11 +117,17 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                             fontSize = 24.sp
                         )
                         TextField(
-                            value = "",
-                            onValueChange = {},
+                            value = nameState.value,
+                            onValueChange = {
+                                nameState.value = it
+                            },
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .padding(top = 8.dp)
+                                .padding(top = 8.dp),
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Text,
+                                capitalization = KeyboardCapitalization.Words
+                            )
                         )
                     }
                     Button(
